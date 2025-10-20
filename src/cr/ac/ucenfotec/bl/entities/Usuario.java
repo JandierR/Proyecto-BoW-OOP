e6aff97e24b65d9d4e491d5132974307a66f7091
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2025-10-20 09:48:48 -0600

    Se ha agregado los atributos, getters, setters, constructor, toString a la clase Usuario.java.

--- src/cr/ac/ucenfotec/bl/entities/Usuario.java
+++ src/cr/ac/ucenfotec/bl/entities/Usuario.java
@@ -1,4 +1,69 @@
 package cr.ac.ucenfotec.bl.entities;
 
 public class Usuario {
+    private String nombreCompleto;
+    private String correo;
+    private String contrasena;
+    private String telefono;
+    private String rol;
+
+
+    public Usuario(String nombreCompleto, String correo, String contrasena, String telefono, String rol) {
+        this.nombreCompleto = nombreCompleto;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.telefono = telefono;
+        this.rol = rol;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombreCompleto='" + nombreCompleto + '\'' +
+                ", correo='" + correo + '\'' +
+                ", contrasena='" + contrasena + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", rol='" + rol + '\'' +
+                '}';
+    }
 }
