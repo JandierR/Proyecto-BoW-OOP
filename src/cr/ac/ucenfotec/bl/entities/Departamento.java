17758ce29dfc9d8aa4e66cb1c9e1c3343326f7ad
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2025-10-20 09:53:07 -0600

    Se ha agregado los atributos, getters, setters, constructor, toString a la clase Departamento.java.

--- src/cr/ac/ucenfotec/bl/entities/Departamento.java
+++ src/cr/ac/ucenfotec/bl/entities/Departamento.java
@@ -1,4 +1,52 @@
 package cr.ac.ucenfotec.bl.entities;
 
 public class Departamento {
+
+    //El nombre del departamento debe de ser unico.
+    private String nombreDepartamento;
+    private String descripcion;
+    private String correo;
+
+    //En la logica, debe de haber una relacion en la que cada departamento puede
+    //Tener muchos tickets.
+
+
+    public Departamento(String nombreDepartamento, String descripcion, String correo) {
+        this.nombreDepartamento = nombreDepartamento;
+        this.descripcion = descripcion;
+        this.correo = correo;
+    }
+
+    public String getNombreDepartamento() {
+        return nombreDepartamento;
+    }
+
+    public void setNombreDepartamento(String nombreDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" +
+                "nombreDepartamento='" + nombreDepartamento + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
 }
