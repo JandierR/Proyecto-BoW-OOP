4ab6d6a091da17dbf8bf5503db81b3b581a30d76
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2025-10-20 09:57:06 -0600

    Se ha agregado los atributos, getters, setters, constructor, toString a la clase Ticket.java.

--- src/cr/ac/ucenfotec/bl/entities/Ticket.java
+++ src/cr/ac/ucenfotec/bl/entities/Ticket.java
@@ -1,4 +1,84 @@
 package cr.ac.ucenfotec.bl.entities;
 
 public class Ticket {
+    //El id presenta que el ticket sea unico
+    private int id;
+    private String asunto;
+    private String descripcion;
+    private String estado;
+    private Usuario usuario;
+    private Departamento departamento;
+
+    //El ticket se debe de asociar con un solo usuario.
+    //Tambien se debe de asociar con un solo departamento.
+
+
+    public Ticket(int id, String asunto, String descripcion, String estado, Usuario usuario, Departamento departamento) {
+        this.id = id;
+        this.asunto = asunto;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.usuario = usuario;
+        this.departamento = departamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", asunto='" + asunto + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", estado='" + estado + '\'' +
+                ", usuario=" + usuario +
+                ", departamento=" + departamento +
+                '}';
+    }
 }
