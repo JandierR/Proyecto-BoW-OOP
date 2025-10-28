21b84d19ca3286e0d15c4749561a593b36546021
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2025-10-28 15:38:35 -0600

    DepartamentoManager.java se ha realizado sus metodos de registarDepartamento y su getter para obtener la lista de departamentos.

--- src/cr/ac/ucenfotec/rojas/jandier/bl/logic/DepartamentoManager.java
+++ src/cr/ac/ucenfotec/rojas/jandier/bl/logic/DepartamentoManager.java
@@ -1,4 +1,27 @@
 package cr.ac.ucenfotec.rojas.jandier.bl.logic;
 
+import cr.ac.ucenfotec.rojas.jandier.bl.entities.Departamento;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepartamentoManager {
+
+    List<Departamento> listaDepartamento;
+
+    public DepartamentoManager() {
+
+        listaDepartamento = new ArrayList<>();
+    }
+
+    public void registrarDepartamento(String nombreDepartamento, String descripcion, String correo) {
+
+        //El nombre del departamento debe de ser unico
+        listaDepartamento.add(new Departamento(nombreDepartamento, descripcion, correo));
+
+    }
+
+    public List<Departamento> getListaDepartamento() {
+        return listaDepartamento;
+    }
 }
