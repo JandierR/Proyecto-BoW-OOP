6919799377eba6f0e4f17673e31226eaecc31f78
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2025-10-30 09:52:30 -0600

    Comentarios de recordatorio futuro trabajo con GestorDiccionarioEmocional y GestorDiccionarioTecnico

--- src/cr/ac/ucenfotec/rojas/jandier/bl/logic/GestorDiccionarioEmocional.java
+++ src/cr/ac/ucenfotec/rojas/jandier/bl/logic/GestorDiccionarioEmocional.java
@@ -1,4 +1,10 @@
 package cr.ac.ucenfotec.rojas.jandier.bl.logic;
 
 public class GestorDiccionarioEmocional {
+    //Con base en lo que el profesor ha indicado,
+    //este Gestor de diccionario no debe de llevar nada en el primer avance,
+    //ya que su implementacion requiere parte del analisis BoW, por lo que, no toma
+    //sitio en este primer avance.
+    //Sin embargo, desde mi logica, este diccionario debe de ir tomado de la mano con
+    //la clase PalabraEmocional, lo cual debo de implementar y estructurar luego en siguientes avances.
 }
--- src/cr/ac/ucenfotec/rojas/jandier/bl/logic/GestorDiccionarioTecnico.java
+++ src/cr/ac/ucenfotec/rojas/jandier/bl/logic/GestorDiccionarioTecnico.java
@@ -1,4 +1,10 @@
 package cr.ac.ucenfotec.rojas.jandier.bl.logic;
 
 public class GestorDiccionarioTecnico {
+    //Con base en lo que el profesor ha indicado,
+    //este Gestor de diccionario no debe de llevar nada en el primer avance,
+    //ya que su implementacion requiere parte del analisis BoW, por lo que, no toma
+    //sitio en este primer avance.
+    //Sin embargo, desde mi logica, este diccionario debe de ir tomado de la mano con
+    //la clase PalabraTecnica, lo cual debo de implementar y estructurar luego en siguientes avances.
 }
